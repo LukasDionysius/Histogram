ae84db197c85eb71216e51777478306c9cb089b4
LukasDionysius <lukasdmichaels@gmail.com>
2018-02-26 20:51:12 -0500

    Update with pseudocode

--- HistogramDriver.java
+++ HistogramDriver.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 
 public class HistogramDriver {
   public static void main(String[] args) {
+    
+    try{ // Look for file
+
+      // Create histogram
+      // Read in data
+
+      // BEGIN LOOP: Some loop that reads the number on the line
+      // Read off first character of the number
+      // Submit first character to histogram class
+      // END LOOP
+
+      // Print histogram
+    }
+    catch{ // If cant find file
+      // Error handler
+    }
 
   }
 }
